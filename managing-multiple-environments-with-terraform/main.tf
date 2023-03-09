af7d48a1d2e04a0b5ec13307bef1a8fbba8c72c3
arjunsreepad <arjunsreepad@gmail.com>
2023-03-09 20:35:57 +0400

    managing-multiple-environments-with-terraform

--- managing-multiple-environments-with-terraform/main.tf
+++ managing-multiple-environments-with-terraform/main.tf
@@ -2,13 +2,13 @@
 provider "aws" {
   region = "us-west-2"
 }
-#resource "aws_instance" "web" {
-#  ami = var.ami_id
-#  instance_type = var.instance_type
-#  tags = {
-#    Name = var.instance_name
-#  }
-#}
+resource "aws_instance" "web" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+  tags = {
+    Name = var.instance_name
+  }
+}
 
 # Create a new security group
 resource "aws_security_group" "instance" {
